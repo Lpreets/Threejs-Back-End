280cc183cce68ffbeb54d48f10a0eef176c94178
Lpreet <lpreet.nerd@gmail.com>
2024-01-27 20:46:03 +0100

    Changed app.use route to the one on AWS

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(cors(corsOptions));
 app.use(express.json({ limit: "50mb" }));
 
-app.use("/api/v1/dalle", dalleRoutes);
+app.use("/", dalleRoutes);
 
 app.get("/", (req, res) => {
     res.status(200).json({ message: "Hello from DALL.E" });
